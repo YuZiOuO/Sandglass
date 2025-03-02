07f02a36abaec479a2e1f87292a70ffb4911175c
kanata <cyz050312@gmail.com>
2025-03-02 17:13:27 +0800

    chore: CI config

--- api/tests/config.py
+++ api/tests/config.py
@@ -1,3 +1,3 @@
 # DO NOT PUT DATABASE NAME IN URI
-TEST_DB_URI = "mongodb://pytest:pwd@localhost/testDB"
-TEST_DB_DATABASE_NAME = "testDB"
+TEST_DB_URI = "mongodb://test_user:pwd@localhost"
+TEST_DB_DATABASE_NAME = "test_db"
