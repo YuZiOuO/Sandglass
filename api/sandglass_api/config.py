958cf4791f94f2022426be763d253e7576cda8b4
kanata <cyz050312@gmail.com>
2025-04-01 23:30:43 +0800

    tests: fix incorrect import in config.py

--- api/sandglass_api/config.py
+++ api/sandglass_api/config.py
@@ -1,14 +1,13 @@
-from sandglass_api.models.access_control import AccessControlList
-from sandglass_api.models.resource import Resource
+from sandglass_api.models.project import Project
 from sandglass_api.models.user import User
 
 # DO NOT PUT DATABASE NAME IN URI
 DB_URI = "mongodb+srv://pyuser:rT9yvudfGwdF22h6@cluster0.bcpu2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DB_DATABASE_NAME = "sandglass"
 
-# This list records all documents,for the sake of tests
+# This list records all documents,for the sake of local tests
 # (the collections listed will be dropped when starting a test)
-ALL_DOCUMENTED_TYPE = [User, Resource, AccessControlList]
+ALL_DOCUMENTED_TYPE = [User, Project]
 
 JWT_EXPIRE_TIME = 3600  # unit:second
 JWT_INVALIDATE_FRESHNESS_FACTOR = 0.2  # If a token is still valid for LESS than 80%(1-20%) of its lifetime, it will be set as not fresh
