f8deb49373c4d842c9b3a97ef66e9864a8ab96e1
kanata <cyz050312@gmail.com>
2025-10-10 15:53:44 +0800

    chore: openapi auto generate

--- api/src/app.exception.ts
+++ api/src/app.exception.ts
@@ -1,9 +1,29 @@
-import { HttpException } from '@nestjs/common';
+import { applyDecorators, HttpException } from '@nestjs/common';
+import { ApiResponse, ApiResponseOptions } from '@nestjs/swagger';
 
 type APIErrorDetail = { message: string; code: string };
 
 export class APIException extends HttpException {
+  _status: number;
+  _detail: APIErrorDetail;
+
   constructor(detail: APIErrorDetail, status: number, cause?: Error) {
     super(detail, status, cause ? { cause: cause } : undefined);
+    this._status = status;
+    this._detail = detail;
   }
+
+  get apiResponseOptions(): ApiResponseOptions {
+    return {
+      status: this._status,
+      description: this._detail.message,
+    };
+  }
+}
+
+type Constructor<T> = new (...args: any[]) => T;
+
+export function generateApiResponse(e: Constructor<APIException>) {
+  const instance = new e();
+  return applyDecorators(ApiResponse(instance.apiResponseOptions));
 }
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from '@nestjs/swagger';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Sandglass example')
+    .setDescription('The Sandglass API description')
+    .setVersion('0.0')
+    .build();
+  const options: SwaggerDocumentOptions = {
+    autoTagControllers: true,
+  };
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, documentFactory, { explorer: true });
+
+  const outputPath = './openapi.json';
+  fs.writeFileSync(outputPath, JSON.stringify(documentFactory(), null, 2));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
