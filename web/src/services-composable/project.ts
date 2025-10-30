111da05fc6aaf4832b358c1434a9afc65accb5b2
kanata <cyz050312@gmail.com>
2025-10-30 14:46:31 +0800

    refactor: project composables

---
+++ web/src/services-composable/project.ts
@@ -0,0 +1,33 @@
+import { useQuery } from '@tanstack/vue-query'
+import { useAccessToken } from './firebase'
+import { ProjectApi } from '@/api'
+import type { Ref } from 'vue'
+
+const projectApi = new ProjectApi()
+
+export function useProjectsQuery() {
+  return useQuery({
+    queryKey: ['project'],
+    queryFn: async () => {
+      const res = await projectApi.listProject({
+        headers: { Authorization: 'Bearer ' + (await useAccessToken()) },
+      })
+
+      return res.data
+    },
+  })
+}
+
+export function useProjectQuery(projectId: Ref<string>) {
+  return useQuery({
+    queryKey: ['calendar', projectId.value],
+    queryFn: async (context) => {
+      const id = context.queryKey[1]
+      const res = await projectApi.getProject(id, {
+        headers: { Authorization: 'Bearer ' + (await useAccessToken()) },
+      })
+
+      return res.data
+    },
+  })
+}
