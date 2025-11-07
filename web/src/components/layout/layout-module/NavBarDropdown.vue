b57d6d9ab23ade37828a7a26037257ca8f8cff3a
kanata <cyz050312@gmail.com>
2025-11-07 13:15:08 +0800

    fix: correct NavBarDropdown login state check and update NavBarMenu project path

--- web/src/components/layout/layout-module/NavBarDropdown.vue
+++ web/src/components/layout/layout-module/NavBarDropdown.vue
@@ -1,6 +1,6 @@
 <template>
-  <n-dropdown :disabled="!isLogined" :options="dropdownOptions" @select="handleDropdownSelect">
-    <n-button v-if="isLogined" tertiary type="primary"> 我的 </n-button>
+  <n-dropdown :disabled="uid === null" :options="dropdownOptions" @select="handleDropdownSelect">
+    <n-button v-if="uid !== null" tertiary type="primary"> 我的 </n-button>
     <RouterLink v-else to="/login">
       <n-button tertiary type="primary"> 登录 </n-button>
     </RouterLink>
@@ -8,30 +8,37 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
 import { RouterLink } from 'vue-router'
 import { renderIcon } from '@/util'
-import { NButton, NDropdown } from 'naive-ui'
+import { NButton, NDropdown, useMessage, type MessageReactive } from 'naive-ui'
 import {
   Pencil as EditIcon,
   LogOutOutline as LogoutIcon,
   PersonCircleOutline as UserIcon,
 } from '@vicons/ionicons5'
 import { useAuthenticationStore } from '@/stores/authentication'
-import pinia from '@/stores'
+import { storeToRefs } from 'pinia'
 
-const authenticationStore = useAuthenticationStore(pinia)
-const isLogined = ref<boolean>(false)
-onMounted(async () => {
-  isLogined.value = authenticationStore.uid !== null
-})
+const authStore = useAuthenticationStore()
+const { uid } = storeToRefs(authStore)
+
+const message = useMessage()
+let logoutMessage: MessageReactive | null = null
+
 async function handleDropdownSelect(key: string) {
   if (key === 'logout') {
+    logoutMessage = message.create('正在注销...', { type: 'loading' })
     try {
-      await authenticationStore.logout()
+      await authStore.logout()
+      logoutMessage.content = '注销成功'
+      logoutMessage.type = 'success'
+      logoutMessage.duration = 2
     } catch (e) {
       console.log(e)
-      throw e
+      logoutMessage.content = '注销失败,请参阅控制台'
+      logoutMessage.type = 'error'
+      logoutMessage.duration = 2
+      logoutMessage.keepAliveOnHover = true
     }
   }
 }
