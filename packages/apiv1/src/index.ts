93456ab7a239404ebd863f29fce30aa1f57a4e79
kanata <cyz050312@gmail.com>
2026-01-30 17:22:20 +0800

    add two packages

---
+++ packages/apiv1/src/index.ts
@@ -0,0 +1,19 @@
+import { serve } from "@hono/node-server";
+import { PrismaClient } from "@sandglass/schema/prisma/client.js";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
