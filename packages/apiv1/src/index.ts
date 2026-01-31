7a35d9b57b2f3bf3081d371aeb30399568009c82
kanata <cyz050312@gmail.com>
2026-01-31 16:41:20 +0800

    successfully integrate with prismaClient

--- packages/apiv1/src/index.ts
+++ packages/apiv1/src/index.ts
@@ -1,10 +1,25 @@
+import { zValidator } from "@hono/zod-validator";
 import { PrismaClient } from "@sandglass/schema/generated/prisma/client";
+import { AttendanceRecordCreateInputSchema } from "@sandglass/schema/generated/zod";
 import { Hono } from "hono";
 
 const app = new Hono();
+const db = new PrismaClient({
+  accelerateUrl: "localhost",
+});
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
+const routes = app.post(
+  "/attendanceRecord",
+  zValidator("form", AttendanceRecordCreateInputSchema),
+  (c) => {
+    const data = c.req.valid("form");
+    return c.json({}, 201);
+  },
+);
+
 export default app;
+export type AppType = typeof routes;
