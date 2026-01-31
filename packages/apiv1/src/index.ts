4051c1c63c6181f88446da2ec1b34af68f0a9e10
kanata <cyz050312@gmail.com>
2026-01-31 16:58:12 +0800

    add firebase auth middleware

--- packages/apiv1/src/index.ts
+++ packages/apiv1/src/index.ts
@@ -1,11 +1,40 @@
 import { zValidator } from "@hono/zod-validator";
 import { PrismaClient } from "@sandglass/schema/generated/prisma/client";
 import { AttendanceRecordCreateInputSchema } from "@sandglass/schema/generated/zod";
+import { initializeApp } from "firebase-admin";
+import { DecodedIdToken } from "firebase-admin/lib/auth/token-verifier";
 import { Hono } from "hono";
 
-const app = new Hono();
+type Env = {
+  Variables: {
+    user: DecodedIdToken;
+    uid: string;
+  };
+};
+
+const app = new Hono<Env>();
 const db = new PrismaClient({
   accelerateUrl: "localhost",
+});
+
+const firebase = initializeApp();
+const firebaseAuth = firebase.auth();
+
+app.use(async (c, next) => {
+  const header = c.req.header("Authorization");
+  if (!header || !header.startsWith("Bearer ")) {
+    return c.json({ error: "Missing or malformed access token" }, 401);
+  }
+
+  const idToken = header.split(" ")[1];
+  try {
+    const decodedIdToken = await firebaseAuth.verifyIdToken(idToken);
+    c.set("user", decodedIdToken);
+    c.set("uid", decodedIdToken.uid);
+    await next();
+  } catch (e) {
+    return c.json({ success: false, error: e });
+  }
 });
 
 app.get("/", (c) => {
