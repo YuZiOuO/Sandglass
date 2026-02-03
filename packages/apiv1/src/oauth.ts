1d351ff95d63160992988980554f12927d9d22f9
kanata <cyz050312@gmail.com>
2026-02-03 19:28:30 +0800

    refactor: migrate github oauth

--- packages/apiv1/src/oauth.ts
+++ packages/apiv1/src/oauth.ts
@@ -4,11 +4,23 @@ import { google } from "googleapis";
 import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
 import { db } from "./db";
+import { fetch } from "bun";
 
 const googleOAuthCallbackSchema = z.object({
   code: z.string(), // Authorization Code
   state: z.string(), // contains uid, for convinience and CSRF
   error: z.string().optional(),
+});
+
+const githubOAuthCallbackSchema = z.object({
+  code: z.string(),
+  state: z.string(),
+});
+
+const githubOAuthExchangeTokenResponseSchema = z.object({
+  access_token: z.string(),
+  scope: z.string(),
+  token_type: z.string(),
 });
 
 export const OAuthPublicRoutes = factory
@@ -41,13 +53,65 @@ export const OAuthPublicRoutes = factory
       });
     },
   )
-  .get("/github/callback");
+  .get(
+    "/github/callback",
+    zValidator("query", githubOAuthCallbackSchema),
+    async (c) => {
+      const data = c.req.valid("query");
+      const uid = data.state;
+      if (!uid) {
+        throw Error("Assertion Error.");
+      }
+
+      // Retrive AccessToken
+      const endpoint = "https://github.com/login/oauth/access_token";
+      const body = {
+        ...githubEnvs,
+        code: data.code,
+      };
+
+      const req = await fetch(endpoint, {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+
+      const res = await githubOAuthExchangeTokenResponseSchema.parseAsync(
+        await req.json(),
+      );
+
+      // Store to DB
+      await db.oAuth.upsert({
+        where: {
+          uid: uid,
+        },
+        create: {
+          uid: uid,
+          githubAccessToken: res.access_token,
+        },
+        update: {
+          uid: uid,
+          githubAccessToken: res.access_token,
+        },
+      });
+
+      return;
+    },
+  );
 
 const googleOAuthCli = new google.auth.OAuth2({
   clientId: env.GApis_OAuth2CliId,
   clientSecret: env.GApis_OAuth2CliSecret,
   redirectUri: env.GApis_RedirectURL,
 });
+
+const githubEnvs = {
+  client_id: env.GH_clientId,
+  redirect_uri: env.GH_RedirectURL,
+};
 
 const googleScopes = [
   "https://www.googleapis.com/auth/calendar",
@@ -83,5 +147,27 @@ export const OAuthRoutes = factory
     const res = await localCli.getAccessToken();
     return c.json(res.token ?? null);
   })
-  .get("/github/authUrl")
-  .get("/github.token");
+  .get("/github/authUrl", async (c) => {
+    const uid = c.var.uid;
+
+    const params = {
+      ...githubEnvs,
+      scope: "user repo:status",
+      state: uid,
+    };
+
+    const endpoint = "https://github.com/login/oauth/authorize";
+    const url = endpoint + "?" + new URLSearchParams(params);
+    return c.json(url);
+  })
+  .get("/github/token", async (c) => {
+    const uid = c.var.uid;
+    const res = await db.oAuth.findUnique({
+      where: {
+        uid: uid,
+      },
+    });
+
+    const token = res?.githubAccessToken ?? null;
+    return c.json(token);
+  });
