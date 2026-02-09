36195414a6e4e77f53bb7fec551f1a9b7d538d41
kanata <cyz050312@gmail.com>
2026-02-09 18:03:18 +0800

    ui: adjust action module layout

--- packages/web/src/views/attandance/AttendanceActionsModule.vue
+++ packages/web/src/views/attandance/AttendanceActionsModule.vue
@@ -1,84 +1,78 @@
 <template>
-  <NButton
-    @click="
-      async () => {
-        await globalQueryClient.refetchQueries({
-          queryKey: ['attendance'],
-        })
-      }
-    "
-  >
-    Debug:刷新所有数据
-  </NButton>
-  <div></div>
+  <NSpace vertical size="large">
+    <!-- Main Button Area -->
+    <NSpace :wrap="false" justify="center" size="large">
+      <NButton
+        size="large"
+        type="primary"
+        :ghost="['IN', undefined].includes(attendanceStatus)"
+        :disabled="['IN', undefined].includes(attendanceStatus)"
+        :loading="attendanceRecordIsCreating === 'IN'"
+        @click="
+          async () => {
+            attendanceRecordCreateRef.json.time = new Date()
+            attendanceRecordCreateRef.json.type = 'IN'
+            attendanceRecordCreate.mutate(attendanceRecordCreateRef)
+          }
+        "
+        >{{ attendanceStatus === 'PAUSE' ? '恢复' : '打上班卡' }}</NButton
+      >
+      <NButton
+        size="large"
+        type="warning"
+        :ghost="attendanceStatus !== 'IN'"
+        :disabled="attendanceStatus !== 'IN'"
+        :loading="attendanceRecordIsCreating === 'PAUSE'"
+        @click="
+          async () => {
+            attendanceRecordCreateRef.json.time = new Date()
+            attendanceRecordCreateRef.json.type = 'PAUSE'
+            attendanceRecordCreate.mutate(attendanceRecordCreateRef)
+          }
+        "
+        >暂停</NButton
+      >
+      <NPopconfirm
+        @positive-click="
+          async () => {
+            attendanceRecordCreateRef.json.time = new Date()
+            attendanceRecordCreateRef.json.type = 'OUT'
+            attendanceRecordCreate.mutate(attendanceRecordCreateRef)
+          }
+        "
+      >
+        <template #trigger>
+          <NButton
+            size="large"
+            type="error"
+            :ghost="['OUT', undefined].includes(attendanceStatus)"
+            :disabled="['OUT', undefined].includes(attendanceStatus)"
+            :loading="attendanceRecordIsCreating === 'OUT'"
+            >打下班卡</NButton
+          >
+        </template>
+        您将签出。当前时间:{{ useNow() }}
+      </NPopconfirm>
+    </NSpace>
 
-  <div class="flex-around">
-    <NButton
-      type="primary"
-      :ghost="['IN', undefined].includes(attendanceStatus)"
-      :disabled="['IN', undefined].includes(attendanceStatus)"
-      :loading="attendanceRecordIsCreating === 'IN'"
-      @click="
-        async () => {
-          attendanceRecordCreateRef.json.time = new Date()
-          attendanceRecordCreateRef.json.type = 'IN'
-          attendanceRecordCreate.mutate(attendanceRecordCreateRef)
-        }
-      "
-      >{{ attendanceStatus === 'PAUSE' ? '恢复' : '打上班卡' }}</NButton
-    >
-    <NButton
-      type="warning"
-      :ghost="attendanceStatus !== 'IN'"
-      :disabled="attendanceStatus !== 'IN'"
-      :loading="attendanceRecordIsCreating === 'PAUSE'"
-      @click="
-        async () => {
-          attendanceRecordCreateRef.json.time = new Date()
-          attendanceRecordCreateRef.json.type = 'PAUSE'
-          attendanceRecordCreate.mutate(attendanceRecordCreateRef)
-        }
-      "
-      >暂停</NButton
-    >
-    <NPopconfirm
-      @positive-click="
-        async () => {
-          attendanceRecordCreateRef.json.time = new Date()
-          attendanceRecordCreateRef.json.type = 'OUT'
-          attendanceRecordCreate.mutate(attendanceRecordCreateRef)
-        }
-      "
-    >
-      <template #trigger>
+    <!-- Sub Button Area -->
+    <NFlex :align="'center'" :wrap="false">
+      <NInput placeholder="事由(选填)" style="flex: 1"> </NInput>
+      <NSpace size="small" :wrap="false">
+        <NButton circle> 补卡 </NButton>
+        <NButton circle> 请假 </NButton>
         <NButton
-          type="error"
-          :ghost="['OUT', undefined].includes(attendanceStatus)"
-          :disabled="['OUT', undefined].includes(attendanceStatus)"
-          :loading="attendanceRecordIsCreating === 'OUT'"
-          >打下班卡</NButton
+          circle
+          @click="
+            () => {
+              attendanceTargetUpdate.mutate(attendanceTargetUpdateRef)
+            }
+          "
+          >修改目标</NButton
         >
-      </template>
-      您将签出。当前时间:{{ useNow() }}
-    </NPopconfirm>
-  </div>
-
-  <NInput v-model:value="attendanceRecordCreateRef.json.summary" placeholder="事由"></NInput>
-
-  <NInputNumber
-    :placeholder="'输入要修改的目标值'"
-    v-model:value="attendanceTargetUpdateRef.json.timeMs"
-  >
-  </NInputNumber>
-  <NButton
-    @click="
-      () => {
-        attendanceTargetUpdate.mutate(attendanceTargetUpdateRef)
-      }
-    "
-    >修改目标</NButton
-  >
-  
+      </NSpace>
+    </NFlex>
+  </NSpace>
 </template>
 
 <script setup lang="ts">
@@ -92,8 +86,7 @@ import {
   type AttendanceTargetUpdateDTO,
 } from '@/services-composable/attendance-target'
 import { computed, ref } from 'vue'
-import { NButton, NPopconfirm,NInput,NInputNumber } from 'naive-ui'
-import { globalQueryClient } from '@/services-composable'
+import { NButton, NInput, NPopconfirm, NSpace, NFlex } from 'naive-ui'
 import { useNow } from '@vueuse/core'
 
 const attendanceRecordLatest = useAttendaceRecordQuery('latest')
@@ -118,10 +111,4 @@ const attendanceTargetUpdate = useAttendanceTargetUpdateMutate()
 const attendanceTargetUpdateRef = ref<AttendanceTargetUpdateDTO>({ json: { timeMs: 0 } })
 </script>
 
-<style scoped>
-.flex-around {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-}
-</style>
+<style scoped></style>
