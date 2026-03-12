2821aca64fc64f7d03c7333c4e3f77431a0e964d
kanata <cyz050312@gmail.com>
2026-03-12 16:31:17 +0800

    feat: add dropdown for task display

--- packages/web/src/views/project/components/TaskDisplayComponent.vue
+++ packages/web/src/views/project/components/TaskDisplayComponent.vue
@@ -1,79 +1,83 @@
 <template>
   <div v-if="item">
     <NSpin :show="updating">
-      <NCard :size="'small'" hoverable>
-        <n-thing>
-          <template #header>
-            <NH5
-              style="margin: 0; margin-right: 5px"
-              prefix="bar"
-              :type="completed ? 'success' : 'info'"
-            >
-              <NText :delete="completed">
-                {{ item.title }}
-              </NText>
-            </NH5>
-          </template>
-          <template #header-extra>
-            <NSpace>
-              <n-checkbox
-                :checked="updating ? !completed : completed"
-                :indeterminate="false"
-                :disabled="updating"
-                @update:checked="
-                  (checked: boolean) => {
+      <NDropdown trigger="click" :options="dropdownOptions" @select="handleDropdownSelect">
+        <NCard :size="'small'" hoverable>
+          <n-thing>
+            <template #header>
+              <NH5
+                style="margin: 0; margin-right: 5px"
+                prefix="bar"
+                :type="completed ? 'success' : 'info'"
+              >
+                <NText :delete="completed">
+                  {{ item.title }}
+                </NText>
+              </NH5>
+            </template>
+            <template #header-extra>
+              <NSpace>
+                <n-checkbox
+                  :checked="updating ? !completed : completed"
+                  :indeterminate="false"
+                  :disabled="updating"
+                  @update:checked="
+                    (checked: boolean) => {
+                      const patchedTask: googleTask = {
+                        id: item?.id,
+                        status: checked ? 'completed' : 'needsAction',
+                      }
+                      patchHook.mutate({ data: patchedTask, tasklistId: tasklistId! })
+                    }
+                  "
+                >
+                </n-checkbox>
+              </NSpace>
+            </template>
+            <template #description v-if="item.notes">
+              {{ item.notes }}
+            </template>
+            <template #footer v-if="item.due">
+              <NPopconfirmWithHook
+                :loading="patchHook.isPending.value"
+                @positive-click="
+                  () => {
                     const patchedTask: googleTask = {
                       id: item?.id,
-                      status: checked ? 'completed' : 'needsAction',
+                      due: deadlineDateModel
+                        ? new Date(deadlineDateModel).toISOString()
+                        : undefined,
                     }
-                    patchHook.mutate({ data: patchedTask, tasklistId: tasklistId! })
+                    patchHook.mutate({
+                      data: patchedTask,
+                      tasklistId: tasklistId!,
+                    })
                   }
                 "
               >
-              </n-checkbox>
-            </NSpace>
-          </template>
-          <template #description v-if="item.notes">
-            {{ item.notes }}
-          </template>
-          <!-- <template #default> </template> -->
-          <template #footer v-if="item.due">
-            <NPopconfirmWithHook
-              :loading="patchHook.isPending.value"
-              @positive-click="
-                () => {
-                  const patchedTask: googleTask = {
-                    id: item?.id,
-                    due: deadlineDateModel ? new Date(deadlineDateModel).toISOString() : undefined,
-                  }
-                  patchHook.mutate({
-                    data: patchedTask,
-                    tasklistId: tasklistId!,
-                  })
-                }
-              "
-            >
-              <template #trigger>
-                <NTag :bordered="false">
-                  <template #icon>
-                    <NIcon :size="'medium'">
-                      <CalendarOutline />
-                    </NIcon>
-                  </template>
-                  {{ new Date(item.due).toLocaleDateString() }}
-                </NTag>
-              </template>
-              <template #form>
-                <NDatePicker
-                  v-model:value="deadlineDateModel"
-                  clearable
-                  placeholder="截止日期（留空将清除）"
-                />
-              </template>
-            </NPopconfirmWithHook>
-          </template>
-        </n-thing>
-      </NCard>
+                <template #description> 修改截止日期 </template>
+                <template #trigger>
+                  <NTag :bordered="false" @click.stop>
+                    <template #icon>
+                      <NIcon :size="'medium'">
+                        <CalendarOutline />
+                      </NIcon>
+                    </template>
+                    {{ new Date(item.due).toLocaleDateString() }}
+                  </NTag>
+                </template>
+                <template #form>
+                  <NDatePicker
+                    v-model:value="deadlineDateModel"
+                    clearable
+                    placeholder="截止日期（留空将清除）"
+                  />
+                </template>
+              </NPopconfirmWithHook>
+            </template>
+          </n-thing>
+        </NCard>
+      </NDropdown>
     </NSpin>
   </div>
 </template>
@@ -94,6 +98,8 @@ import {
   NH5,
   NIcon,
   NDatePicker,
+  NDropdown,
+  type DropdownOption,
 } from 'naive-ui'
 import { CalendarOutline } from '@vicons/ionicons5'
 import { computed, ref, watchEffect } from 'vue'
@@ -116,4 +122,34 @@ const deadlineDateModel = ref<number>()
 watchEffect(() => {
   deadlineDateModel.value = props.item?.due ? new Date(props.item?.due).getTime() : undefined
 })
+
+const dropdownOptions = [
+  {
+    key: 'delete',
+    label: '删除',
+  },
+  {
+    key: 'goto',
+    label: '在Google Tasks中查看',
+  },
+] as const satisfies DropdownOption[]
+
+type DropdownKey = (typeof dropdownOptions)[number]['key']
+const handleDropdownSelect = (key: DropdownKey) => {
+  switch (key) {
+    case 'delete':
+      console.log('To be implemented')
+      break
+    case 'goto':
+      const url = props.item?.webViewLink
+      if (url) {
+        window.open(url, '_blank')
+      } else {
+        throw new Error('Assertion Error')
+      }
+      break
+    default:
+      throw new Error(`Assertion Error: ${key as never}`);
+  }
+}
 </script>
