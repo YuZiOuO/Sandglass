13c0a810820990cd594d96f2c6f1a20ca49d14ef
kanata <cyz050312@gmail.com>
2026-03-21 23:16:09 +0800

    fix: EventsTimeline dropdown callbacks
    feat: ux for EventsTimeline Loading
    ui: change styles for url

--- packages/web/src/views/common/EventsTimeline.vue
+++ packages/web/src/views/common/EventsTimeline.vue
@@ -2,6 +2,9 @@
   <n-empty description="你什么也找不到" v-if="!(filteredMergedEvents.length !== 0)" />
 
   <NTimeline v-else>
+    <NTimelineItem v-if="loading">
+      <n-spin size="small" />
+    </NTimelineItem>
     <NTimelineItem
       v-for="r in filteredMergedEvents"
       :key="r.timestamp"
@@ -12,21 +15,25 @@
     >
       <template #header>
         <component
-          :is="r.dropdown ? NDropdown : 'div'"
+          :is="r.actions?.length ? NDropdown : 'div'"
           v-bind="
-            r.dropdown
+            r.actions?.length
               ? {
                   trigger: 'hover',
                   placement: 'right',
-                  options: r.dropdown.options,
-                  onSelect: r.dropdown.callback,
+                  options: r.actions,
+                  onSelect: (key: string) => r.actions?.find((a) => a.key === key)?.onSelect(),
                 }
               : {}
           "
         >
-          <NText style="cursor: pointer">
-            {{ r.header }}
-            <a v-if="r.url" :href="r.url" target="_blank">🔗</a>
+          <NText>
+            <span style="cursor: pointer">{{ r.header }}</span>
+            <NButton quaternary v-if="r.url" text tag="a" :href="r.url" target="_blank">
+              <template #icon>
+                <NIcon size="tiny"><OpenOutline /></NIcon>
+              </template>
+            </NButton>
           </NText>
         </component>
       </template>
@@ -41,12 +48,16 @@
 </template>
 
 <script setup lang="ts">
-import { useAttendanceRecordQuery } from '@/services-composable/attendance-record'
+import {
+  useAttendanceRecordDeleteMutate,
+  useAttendanceRecordQuery,
+} from '@/services-composable/attendance-record'
 import { useGithubListRepoCommitsQuery } from '@/services-composable/third-party/github'
-import { NTimeline, NTimelineItem, NEmpty, NDropdown, NText, NIcon } from 'naive-ui'
+import { OpenOutline } from '@vicons/ionicons5'
+import { NButton, NDropdown, NEmpty, NIcon, NText, NTimeline, NTimelineItem, NSpin } from 'naive-ui'
 import { computed } from 'vue'
-import { attendanceRecord2Events, commits2Events, tasks2Events } from './EventsTimeline'
 import { useGoogleTasksQuery } from '@/services-composable/third-party/google-tasks'
+import { attendanceRecord2Events, commits2Events, tasks2Events } from './EventsTimeline'
 
 /**
  * if undefined, events of that type will not be displayed
@@ -86,6 +97,7 @@ const presetStartTimestamp = computed(() => {
   return relativeDate?.getTime()
 })
 
+// Raw Data
 const attendanceRecords = useAttendanceRecordQuery(
   () => props.displayPreset,
   () => props.config.attendance.projectId,
@@ -96,7 +108,18 @@ const commits = useGithubListRepoCommitsQuery(
 )
 const tasks = useGoogleTasksQuery(() => props.config.googleTask?.TasklistId, true)
 
-const attendanceEvents = computed(() => attendanceRecord2Events(attendanceRecords.data.value))
+// UI state
+const loading = computed(
+  () => attendanceRecords.isLoading.value || commits.isLoading.value || tasks.isLoading.value,
+)
+
+// Events
+const { mutate: deleteAttendance } = useAttendanceRecordDeleteMutate()
+const attendanceEvents = computed(() =>
+  attendanceRecord2Events(attendanceRecords.data.value, {
+    onDelete: (id) => deleteAttendance(id),
+  }),
+)
 const commitsEvents = computed(() =>
   commits2Events(
     commits.data.value?.filter((item) =>
@@ -107,12 +130,12 @@ const commitsEvents = computed(() =>
   ),
 )
 const tasksEvents = computed(() => tasks2Events(tasks.data.value?.items))
-
 const mergedEvents = computed(() => {
   const merged = [...attendanceEvents.value, ...commitsEvents.value, ...tasksEvents.value]
   return merged.sort((a, b) => a.timestamp - b.timestamp)
 })
 
+// Merged
 const filteredMergedEvents = computed(() =>
   mergedEvents.value.filter(
     (item) => !presetStartTimestamp.value || item.timestamp > presetStartTimestamp.value,
