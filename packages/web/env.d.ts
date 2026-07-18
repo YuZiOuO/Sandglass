fcb6e7498db167cbe87f3b5e9106725adbd6c76b
kanata <cyz050312@gmail.com>
2026-07-18 20:47:59 +0800

    init: delete all files

--- packages/web/env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-/// <reference types="vite-svg-loader" />
-/// <reference types="@types/gapi.client.tasks" />
-/// <reference types="@types/gapi.client.calendar" />
-interface ImportMetaEnv {
-  readonly SG_WEB_API_BASEURL: string
-  readonly SG_WEB_SENTRY_DSN?: string
-}
--- packages/web/src/assets/index.ts
+++
@@ -1,6 +0,0 @@
-import IconGoogleCalendar from './google-calendar.svg?component'
-import IconGoogleTasks from './google-tasks.svg'
-import IconGoogleTasksOutline from './google-tasks-outline.svg'
-import IconGoogleCalendarOutline from './google-calendar-outline.svg'
-
-export { IconGoogleCalendar, IconGoogleTasks, IconGoogleCalendarOutline, IconGoogleTasksOutline }
